1c6bb40f896306434b4a6797b5b9703bee8aa09c
oxigen <Oxigen96@users.noreply.github.com>
2020-09-28 15:24:06 +0200

    Working link scraper with requests

--- scrap_all_links/scrap.py
+++ scrap_all_links/scrap.py
@@ -1,12 +1,12 @@
-import httplib2
+import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
 # args handling
-# pretty print scraped links
+# | pretty print scraped links
+# |-> parse [done]
 
-http = httplib2.Http()
-status, response = http.request('http://google.com/')
+response = requests.get("https://gesco.bearzi.it/")
 
-for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
-    if link.has_attr('href'):
+for link in BeautifulSoup(response.text, features="html.parser", parse_only=SoupStrainer('a')):
+    if link.has_attr('href') and link['href'].startswith("http"):
         print(link['href'])
