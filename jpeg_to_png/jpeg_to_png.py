2a98b49a759c9ef8f967dd9a2338908e595a4748
Deepak Raj <deepak008@live.com>
2020-09-29 09:35:14 +0530

    minor improvement - try/except added for error handling

--- jpeg_to_png/jpeg_to_png.py
+++ jpeg_to_png/jpeg_to_png.py
@@ -3,5 +3,8 @@ from PIL import Image
 
 input_path = sys.argv[1]
 
-Image.open(input_path).save("Converted.png")
-print("Image Saved")
+try:
+    Image.open(input_path).save("Converted.png")
+    print("\n--------------Image Saved------------\n")
+except Exception:
+    print("\n--------------------File Not Found-------------------\n")
