1de7fc210440c275aa3e77937e6bddce433113da
Ameya Jain <ameiya.jain@hotmail.com>
2020-10-02 00:34:24 +0530

    Flake8 used for clean code

--- Encrypt_PDF/encryptPDF.py
+++ Encrypt_PDF/encryptPDF.py
@@ -1,11 +1,13 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-print("Make sure that the pdf to be encrypted is strode in the same FOLDER as this script")
+print("The file should be in same FOLDER as this script")
 pdfNameInput = input("Enter EXACT name of the PDF in this FOLDER: ")
 pdfName = pdfNameInput + ".pdf"
-pdf = PdfFileReader(pdfName)  #reading the pdf
-write_obj = PdfFileWriter()     # object for writing the file
+# reading the pdf
+pdf = PdfFileReader(pdfName)
+# object for writing the file
+write_obj = PdfFileWriter()
 
 
 # Getting the number of pages and writing each page in the writer object
@@ -13,7 +15,7 @@ for i in range(pdf.getNumPages()):
     page = pdf.getPage(i)
     write_obj.addPage(page)
 
-#Encrypting by a password
+# Encrypting by a password
 password = input("Enter Password for the Encryption to PDF: ")
 write_obj.encrypt(user_pwd=password, owner_pwd=None, use_128bit=True)
 
@@ -21,6 +23,3 @@ new_PDF_Name_Input = input("Enter new PDF name: ")
 new_PDF_Name = new_PDF_Name_Input + '.pdf'
 encrypted_PDF = open(new_PDF_Name, 'wb')
 write_obj.write(encrypted_PDF)
-
-
-print("\nYour PDF has been Succesfully encrypted..... Kudos !!!!\n")
