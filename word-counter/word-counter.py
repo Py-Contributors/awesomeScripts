6c6cf2de326950be33939c4963d8e8d707074d65
p014ri5 <5206906-p014ri5@users.noreply.gitlab.com>
2020-10-01 08:58:17 +0530

    Added README.md

--- word-counter/word-counter.py
+++ word-counter/word-counter.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-Count work frequencies within a file.
-```
-$ word-counter.py shakespeare.md --numWords 4 --maxTuples 3
-=== Sliding Window: 1 ===
-    3473: 'shall'
-    2238: 'would'
-    2153: 'which'
-    2074: 'their'
-=== Sliding Window: 2 ===
-    248: 'exeunt scene'
-    117: 'second lord.'
-    105: 'first lord.'
-    102: 'queen elizabeth.'
-=== Sliding Window: 3 ===
-    36: 'william shakespeare dramatis'
-    34: 'shakespeare dramatis personae'
-    18: 'comes here? enter'
-    14: 'duke's palace enter'
-```
-"""
-
 from collections import Counter
 import argparse
 import re
