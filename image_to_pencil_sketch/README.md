cdb02a2de8d6e5d089073e9ed50dc942f1ed3b7c
Girish Kulkarni <girish332@pop-os.localdomain>
2020-10-02 21:49:03 +0530

    Added script to convert image to a pencil sketch for issue #112

---
+++ image_to_pencil_sketch/README.md
@@ -0,0 +1,14 @@
+## Image to pencil sketch
+
+Convert the image into a pencil sketch using open cv
+
+## using module
+
+- sys
+- cv2
+
+## How to Use
+
+```bash
+python convert2sketch.py.py <image_path>
+```
---
+++ image_to_pencil_sketch/convert2sketch.py
@@ -0,0 +1,21 @@
+import os 
+import sys
+import cv2
+
+def dodgeV2(x,y):
+    return cv2.divide(x,255-y,scale = 256)
+
+
+if len(sys.argv) > 1:
+
+    if(os.path.exists(sys.argv[1])):
+        img_rgb = cv2.imread(sys.argv[1])
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+        img_gray_inv = cv2.bitwise_not(img_gray)
+        img_blur = cv2.GaussianBlur(img_gray_inv, ksize=(21, 21),sigmaX=0, sigmaY=0)
+        final_img = dodgeV2(img_gray,img_blur)
+        cv2.imwrite('converted_pencil_sketch.jpg',final_img)
+
+
+
