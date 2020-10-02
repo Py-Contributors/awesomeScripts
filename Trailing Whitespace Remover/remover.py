86b289dfe8606cd5d3ed4015832715ddb3275e44
Aarav M <aarav.mahesh@gmail.com>
2020-10-02 10:41:55 -0500

    added comments for readability

--- Trailing Whitespace Remover/remover.py
+++ Trailing Whitespace Remover/remover.py
@@ -1,5 +1,6 @@
 import argparse
 
+# Creating Argument Parser
 parser = argparse.ArgumentParser(
     description="Remove Trailing Whitespaces from files."
 )
@@ -12,9 +13,10 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-for fname in args.files:
+for fname in args.files:  # iterating over each file provided
+    # remove trailing characters from each line
     lines = [i.rstrip() for i in open(fname, "r").readlines()]
     with open(fname, "w"):
-        pass
+        pass  # truncate to remove text from file
     with open(fname, "w") as f:
-        f.write("\n".join(lines))
+        f.write("\n".join(lines))  # write new clean text
