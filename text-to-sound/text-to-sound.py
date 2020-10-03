56ef50b3dec35a4dc85898fc008c4ab1f9293718
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2020-10-03 17:29:39 +0300

    fix failed check is about blank

--- text-to-sound/text-to-sound.py
+++ text-to-sound/text-to-sound.py
@@ -3,6 +3,7 @@ import pygame
 
 from gtts import gTTS
 
+
 def speak(text):
     with io.BytesIO() as file:
         gTTS(text=text, lang="en").write_to_fp(file)
@@ -12,6 +13,7 @@ def speak(text):
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             continue
+
 
 if __name__ == '__main__':
     text = input("What should I say? :")
