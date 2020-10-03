59ce5de77f7520b518fdc3e31c5b1abcc87be4b5
DiptoChakrabarty <diptochuck123@gmail.com>
2020-10-03 16:54:59 +0530

    fixed using flake8

--- ec2_launcher/ec2.py
+++ ec2_launcher/ec2.py
@@ -1,32 +1,33 @@
-import boto3,os
+import boto3
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-#Load env variables
-# Add all these environment variables in your env file
-# format of env file has been provided by adding an empty .env
-access_key= os.getenv("ACCESS_KEY")
-secret_key= os.getenv("SECRET_KEY")
-ami= os.getenv("AMI")
-region= os.getenv("REGION")
-zone= os.getenv("ZONE")
-type= os.getenv("TYPE")
+access_key = os.getenv("ACCESS_KEY")
+secret_key = os.getenv("SECRET_KEY")
+ami = os.getenv("AMI")
+region = os.getenv("REGION")
+zone = os.getenv("ZONE")
+type = os.getenv("TYPE")
 subnet = os.getenv("SUBNET")
+key_name = os.getenv("KEY")
 
-client = boto3.client(service_name='ec2', region_name=region, aws_access_key_id= access_key,aws_secret_access_key= secret_key)
+client = boto3.client(
+    service_name='ec2', region_name=region,
+    aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 
 # Create ec2 resource
-ec2 = boto3.resource('ec2', region_name=region, aws_access_key_id= access_key,aws_secret_access_key= secret_key)
+ec2 = boto3.resource(
+    'ec2', region_name=region, aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key)
 # create an instance
 instance = ec2.create_instances(
-    ImageId = ami,
-    MinCount = 1,
-    MaxCount = {specify max instances needed here},  # here replace brancket with the number of instances you need 
-    InstanceType = type,   
-    KeyName = key_name,  
-    SubnetId = subnet)
+    ImageId=ami,
+    MinCount=1,
+    MaxCount=10,
+    InstanceType=type,
+    KeyName=key_name,
+    SubnetId=subnet)
 
 instance.wait_until_running()
 print("Instance Up and Running")
-
-# To use the script run python3 ec2.py 
