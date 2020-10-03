f1ab36f3b0cc5800605cba69f2217cd85dde6cbf
Monil Nisar <nisarmonil@gmail.com>
2020-10-02 23:47:42 -0700

    Added code. Noted changes to the reademe file.

--- email_sending_script/email_sending_script.py
+++ email_sending_script/email_sending_script.py
@@ -1,75 +1,86 @@
-import smtplib
-import ssl
+import ssl, smtplib, mimetypes, re, getpass
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.audio import MIMEAudio
 from email import encoders
 from email.mime.base import MIMEBase
-import mimetypes
 
+def check_email(email):
+	regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+	if(re.search(regex,email.strip())):  
+		return True
+	else:
+		return False
 
-port = 465
-
+PORT = 465
 print("Welcome to Email sending script!\n \
 Read the README.md file for more details\n\n")
-
-
-senders_email = input("Enter the sender's gmail: ")
-recivers_email = input("Enter the reciever's gmail: ")
-password = input("Enter the serder's gmail password: ")
+	
 context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", PORT, context=context) as server:
+	while True:
+		# checking sender's email and exit from middle of program
+		senders_email = input("Enter the sender's gmail email('' to exit): ").lower()
+		if senders_email=='':
+			break
+		while not check_email(senders_email):
+			senders_email = input("Enter the sender's gmail email correctly('' to exit): ").lower()
+		password = getpass.getpass(prompt="Enter the serder's gmail password: ")
+		try:
+			server.login(senders_email, password)
+			print("Login sucessfull!\n\n")
+		except Exception:
+			print("Login was unsucessfull! Try again! \n\n")
+			continue
+
+		recivers_email = input("Enter the reciever(s) email. Seperate be comma(',')('' to exit): ").lower()
+		if recivers_email=='':
+			break
+		while not all(map(lambda x: check_email(x), recivers_email.split(","))):
+			recivers_email = input("Enter the reciever(s) email correctly. Seperate be comma(',')('' to exit): ")
 
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    try:
-        server.login(senders_email, password)
-        print("Login sucessfull!")
-        subject = input("Enter subject: ")
-        message = input("Enter message: ")
-        msg = MIMEMultipart()
-        msg["Subject"] = subject
-        msg["To"] = recivers_email
-        msg["From"] = senders_email
-        msg.attach(MIMEText(message, "plain"))
-        Attachement = input("Do you want to add attachement?(Y/N): ")
-        if Attachement == "y" or Attachement == "Y":
-            numberOfAttachment = int(input("How many attachement \
-            do you want to add: "))
-            for i in range(numberOfAttachment):
-                path = input(f"Enter absolute path to the attachement {i+1}: ")
-                filename = ""
-                ctype, encoding = mimetypes.guess_type(path)
-                if ctype is None or encoding is not None:
-                    ctype = "application/octet-stream"
-                maintype, subtype = ctype.split("/", 1)
-                if maintype == "text":
-                    fb = open(path)
-                    files = MIMEText(fb.read(), _subtype=subtype)
-                    filename = fb.name
-                    fb.close()
-                elif maintype == "image":
-                    fb = open(path, "rb")
-                    files = MIMEImage(fb.read(), _subtype=subtype)
-                    filename = fb.name
-                    fb.close()
-                elif maintype == "audio":
-                    fp = open(path, "rb")
-                    files = MIMEAudio(fp.read(), _subtype=subtype)
-                    filename = fp.name
-                    fp.close()
-                else:
-                    fp = open(path, "rb")
-                    files = MIMEBase(maintype, subtype)
-                    files.set_payload(fp.read())
-                    filename = fp.name
-                    fp.close()
-                    encoders.encode_base64(files)
-                files.add_header("Content-Disposition",
-                                 "attachment", filename=filename)
-                msg.attach(files)
-        server.send_message(msg)
-        print("Email sent sucessfully!")
-    except Exception:
-        print("Login was unsucessfull! read \
-        the README.md file for instruction")
+		subject = input("Enter e-mail subject: ")
+		message = input("Enter e-mail body: ")
+		msg = MIMEMultipart()
+		msg["Subject"] = subject
+		msg["To"] = recivers_email
+		msg["From"] = senders_email
+		msg.attach(MIMEText(message, "plain"))
+		Attachement = input("\nDo you want to add attachement?(Y/N): ").strip().lower()
+		if Attachement == "y":
+			numberOfAttachment = int(input("How many attachement do you want to add: "))
+			for i in range(numberOfAttachment):
+				path = input(f"Enter absolute path to the attachement {i+1}: ")
+				filename = ""
+				ctype, encoding = mimetypes.guess_type(path)
+				if ctype is None or encoding is not None:
+					ctype = "application/octet-stream"
+				maintype, subtype = ctype.split("/", 1)
+				if maintype == "text":
+					fb = open(path)
+					files = MIMEText(fb.read(), _subtype=subtype)
+					filename = fb.name
+					fb.close()
+				elif maintype == "image" or maintype=="audio":
+					fb = open(path, "rb")
+					files = MIMEImage(fb.read(), _subtype=subtype)
+					filename = fb.name
+					fb.close()
+				else:
+					fp = open(path, "rb")
+					files = MIMEBase(maintype, subtype)
+					files.set_payload(fp.read())
+					filename = fp.name
+					fp.close()
+				encoders.encode_base64(files)
+				files.add_header("Content-Disposition", "attachment", filename=filename)
+				msg.attach(files)
+		server.send_message(msg)
+		print("Email sent!\n\n")
+		
+
+		choice = input("Send another email?(y/n): ").strip().lower()
+		if choice!='y':
+			break
