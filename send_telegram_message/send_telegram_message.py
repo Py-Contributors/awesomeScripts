5698030165634cc941aac72028adb59c3c01862e
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2020-10-04 00:00:50 +0530

    Update send_telegram_message.py

--- send_telegram_message/send_telegram_message.py
+++ send_telegram_message/send_telegram_message.py
@@ -7,7 +7,7 @@ Licensed under MIT license
 from telethon import TelegramClient, sync
 import argparse
 
-#Usage python send_telegram_message.py "username, number or id" "message"
+# Usage python send_telegram_message.py "username, number or id" "message"
 parser = argparse.ArgumentParser(description='Send message to telegram user from terminal')
 parser.add_argument('user', type=str, help='Telegram username, number or id')
 parser.add_argument('msg', type=str, help='Message to send to user')
@@ -15,7 +15,7 @@ args = parser.parse_args()
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
-api_id = 0
+api_id = ""
 api_hash = 'nothing'
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
