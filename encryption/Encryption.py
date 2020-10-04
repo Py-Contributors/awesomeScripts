73271aae7e6b58a396f3dde127d2b7cbb0bb59d7
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2020-10-04 11:56:18 +0530

    Update Encryption.py

--- encryption/Encryption.py
+++ encryption/Encryption.py
@@ -7,10 +7,10 @@ class ImageEncryption:
     """
     this class for image encryption
     """
-    __image_key = Random.new().read(AES.block_size);
-    __init_vector = Random.new().read(AES.block_size);
+    __image_key = Random.new().read(AES.block_size)
+    __init_vector = Random.new().read(AES.block_size)
 
-    def encrypt_image(self,par_image,par_enc_image):
+    def encrypt_image(self, par_image, par_enc_image):
         """
             this function for encrypting image
             args:
@@ -18,15 +18,18 @@ class ImageEncryption:
                 par_enc_image: an image after encryption
             return:none
         """
-        with open(par_image,'rb') as input_file:
+        with open(par_image, 'rb') as input_file:
             input_data = input_file.read()
-        cfb_cipher = AES.new(self.__image_key, AES.MODE_CFB, self.__init_vector)
+        cfb_cipher = AES.new(
+                             self.__image_key,
+                             AES.MODE_CFB,
+                             self.__init_vector)
         enc_data = cfb_cipher.encrypt(bytearray(input_data))
 
         with open(par_enc_image, "wb") as enc_file:
             enc_file.write(enc_data)
 
-    def decrypt_image(self,par_enc_image,par_real_image):
+    def decrypt_image(self, par_enc_image, par_real_image):
         """
         this function for decrypting image
         args:
@@ -37,7 +40,10 @@ class ImageEncryption:
         with open(par_enc_image, "rb") as enc_file:
             enc_data = enc_file.read()
 
-        cfb_decipher = AES.new(self.__image_key, AES.MODE_CFB, self.__init_vector)
+        cfb_decipher = AES.new(
+                               self.__image_key,
+                               AES.MODE_CFB,
+                               self.__init_vector)
         plain_data = cfb_decipher.decrypt(enc_data)
 
         with open("output.jpg", "wb") as output_file:
@@ -49,7 +55,7 @@ class TextEncryption:
         this class for text encryption
         install cryptography.fernet
     """
-    __txt_key = Fernet.generate_key();
+    __txt_key = Fernet.generate_key()
 
     def encrypt_text(self, par_txt):
         """
