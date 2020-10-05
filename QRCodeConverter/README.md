30ee269a6dfbcb3bd4dde3a7c342bcb1dbe7dbb7
dhiganthrao <dhiganth18037@ece.ssn.edu.in>
2020-10-05 12:11:53 +0530

    Edited Script.py to pass flake8 standards.

--- QRCodeConverter/README.md
+++ QRCodeConverter/README.md
@@ -2,17 +2,17 @@
 
 This is a simple Python script aimed at converting long or short website URLs into QR codes to be scanned.
 
-# Requirements
+## Requirements
 
 It is enough to run `pip install -r requirements.txt` to install all the requirements necessary for this script.
 
-# Usage
+## Usage
 
-Simply run `python Script.py` in your terminal and enter the URL to be converted into a QR code. 
+Simply run `python Script.py` in your terminal and enter the URL to be converted into a QR code.
 
-# Screenshots and sample QR codes:
+## Screenshots and sample QR codes
 
 ![Screenshot](Screenshot.png)
 
 QR code for [GitHub](https://www.github.com):  
-![QR Code](QRCode-1.png)
+![QR Code](QRCode-2.png)
--- QRCodeConverter/Script.py
+++ QRCodeConverter/Script.py
@@ -1,14 +1,14 @@
-import pyqrcode 
-import png 
-from pyqrcode import QRCode 
-  
-  
+import pyqrcode
+# import png
+# from pyqrcode import QRCode
+
 
 def QRCodeConverter(url, count):
-
-    qr = pyqrcode.create(url) # Generating the QR code
-    qr.png(f'./QRCodeConverter/QRCode-{count}.png', scale = 6) # Saving the QR code as a PNG
+    qr = pyqrcode.create(url)  # Generating the QR code
+    # Saving the QR code as a PNG
+    qr.png(f'./QRCodeConverter/QRCode-{count}.png', scale=6)
     print("QR Code saved!")
+
 
 choice = 'y'
 count = 0
@@ -16,5 +16,5 @@ while(choice == 'y' or choice == 'yes'):
     count = count + 1
     url = input("Enter the URL to convert into a QR Code: ")
     QRCodeConverter(url, count)
-    choice = input('Do you want to convert another URL? (y/yes for yes, any other key for no): ')
-    
+    print('Do you want to convert another URL?')
+    choice = input('(y/yes for yes, any other key for no): ')
