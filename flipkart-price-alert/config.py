4945e342f47f4462b9c52b3794e88d05ec6be7cd
Ea <51820183+Jade9ja@users.noreply.github.com>
2020-10-08 19:18:42 +0530

    Add flipkart price tracker (#245)
    * added-flipkart-price-alert
    
    * Update README.md
    
    * Update README.md
    
    * Update config.py

---
+++ flipkart-price-alert/config.py
@@ -0,0 +1,13 @@
+headers = {'User-Agent': 'Mozilla/5.0 Chrome/86.0.4240.75'}
+# Enter the URL of the product page on flipkart
+PRODUCT_URL = 'enter_url_here'
+# Enter the threshhold price of the product
+THRESHHOLD = 6969.0
+# Enter your email address
+MY_EMAIL = ''
+# Enter your password (Check readme.md for steps to get app password)
+MY_APP_PASSWORD = ''
+# Enter time delay (60 for 1 minute)
+CHECK_AGAIN = 60 * 30  # 60*30 will be 30 minutes
+# Enter the recipient email
+RECEIVER_EMAIL = ''
