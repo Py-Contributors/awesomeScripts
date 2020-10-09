a1c8497e046bd7bb8f37bdaaa19f3c6eb1629f00
Bruno Bastos <zerobruno@gmail.com>
2020-10-09 02:05:43 -0700

    Adding resizing logic

--- image_resizer/image_resizer.py
+++ image_resizer/image_resizer.py
@@ -1,16 +1,26 @@
 import sys
 import os.path
 from argparse import ArgumentParser
+from PIL import Image
 
 parser = ArgumentParser()
 parser.add_argument('--file', dest='File', help='Input image filename',
                     required=True)
-parser.add_argument('--width', dest='Width', help='Width in pixels')
-parser.add_argument('--height', dest='Height', help='Height in pixels')
-parser.add_argument('--output', dest='Output', default='output',
-                    help='The output folder')
+parser.add_argument('--width', dest='Width', help='Width in pixels',
+                    type=int)
+parser.add_argument('--height', dest='Height', help='Height in pixels',
+                    type=int)
 args = parser.parse_args()
 
 if not args.Width and not args.Height:
     print('Error: Either width or height must be specified')
+    sys.exit(1)
+
+try:
+    input_image = Image.open(args.File)
+    resized_image = input_image.resize((args.Width, args.Height))
+    file_extension = os.path.splitext(args.File)[1]
+    resized_image.save('output.%s' % (file_extension))
+except Exception:
+    print('Error: Could not resize image')
     sys.exit(1)
