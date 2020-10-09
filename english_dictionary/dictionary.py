687fca91615612cf312649b123f85a8f95b28ad7
alovictor <victor.neves@live.com>
2020-10-09 03:46:30 -0400

    formating with flake8

--- english_dictionary/dictionary.py
+++ english_dictionary/dictionary.py
@@ -8,13 +8,13 @@ import difflib as dif
 '''
 
 def dic(word):
-    if word in data:  
+    if word in data:
         return data[word]   # if the word is written correctly
-    elif dif.SequenceMatcher(None, word, dif.get_close_matches(word, data.keys())[0]).ratio()>0.6: #  if the similarity to another word is greater than 0.6
-        answer = input('Did you mean {}? Y or N\n'.format(dif.get_close_matches(word, data.keys())[0])) # asks if the other word is the right word
+    elif dif.SequenceMatcher(None, word, dif.get_close_matches(word, data.keys())[0]).ratio() > 0.6:  # if the similarity to another word is greater than 0.6
+        answer = input('Did you mean {}? Y or N\n'.format(dif.get_close_matches(word, data.keys())[0]))  # asks if the other word is the right word
         answer = answer.upper()
         if answer == 'Y':
-            return data[dif.get_close_matches(word, data.keys())[0]] # if was the right one, returns the meaning
+            return data[dif.get_close_matches(word, data.keys())[0]]  # if was the right one, returns the meaning
         elif answer == 'N':
             return 'Unknown word. Please, try again'
         else:
@@ -28,16 +28,16 @@ if __name__ == '__main__':
     parser.add_argument('word', help='Input word to search')
     args = parser.parse_args()
 
-    data = json.load(open('data.json')) # loads the dictionary
+    data = json.load(open('data.json'))  # loads the dictionary
 
     while True:
         word = args.word
         out = dic(word)
 
-        if isinstance(out, list): #   checks if out is a list of meanings to show line by line
+        if isinstance(out, list):  # checks if out is a list of meanings to show line by line
             for x in range(len(out)):
                 print(f"{x + 1} - " + out[x])
             break
         else:
             print(out)
-            break 
+            break
