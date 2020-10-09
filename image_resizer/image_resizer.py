3a15a38c849c83fb859dfae3a774911d3b3e6b68
Bruno Bastos <zerobruno@gmail.com>
2020-10-09 01:17:51 -0700

    Making file argument required

--- image_resizer/image_resizer.py
+++ image_resizer/image_resizer.py
@@ -3,7 +3,8 @@ import os.path
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('--file', dest='File', help='Input image filename')
+parser.add_argument('--file', dest='File', help='Input image filename',
+                    required=True)
 parser.add_argument('--width', dest='Width', help='Width in pixels')
 parser.add_argument('--height', dest='Height', help='Height in pixels')
 parser.add_argument('--output', dest='Output', default='output',
@@ -12,8 +13,4 @@ args = parser.parse_args()
 
 if not args.Width and not args.Height:
     print('Error: Either width or height must be specified')
-    sys.exit(1)
-
-if args.File and not os.path.isfile(args.File):
-    print('Error: The specified file does not exist')
     sys.exit(1)
