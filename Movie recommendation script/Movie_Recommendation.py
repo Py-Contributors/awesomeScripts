e45259edacf1beae32fadcfc0467bfa3194261b6
Jainchhavi16 <48974211+Jainchhavi16@users.noreply.github.com>
2020-10-11 18:08:43 +0530

    Update Movie_Recommendation.py

--- Movie recommendation script/Movie_Recommendation.py
+++ Movie recommendation script/Movie_Recommendation.py
@@ -1,3 +1,2 @@
-# to be treated as a README File
-
-#This pyhton script scrapes the IMDB site and returns top 5 movies based on emption entered. 
+# to be treated as a README File.
+# This pyhton script scrapes the IMDB site and returns top 5 movies based on emption entered. 
