2d89cfadd6a7da00780647018a84edec14b3bd62
Jainchhavi16 <48974211+Jainchhavi16@users.noreply.github.com>
2020-10-11 17:24:48 +0530

    Update Movie_Recommendation.py

--- Movie recommendation script/Movie_Recommendation.py
+++ Movie recommendation script/Movie_Recommendation.py
@@ -13,42 +13,42 @@ def main(emotion):
 
 	# IMDb Url for Drama genre of
 	# movie against emotion Sad
-	if(emotion == "Sad"):
+	if(emotion == "sad"):
 		urlhere = 'http://www.imdb.com/search/title?genres=drama&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Musical genre of
 	# movie against emotion Disgust
-	elif(emotion == "Disgust"):
+	elif(emotion == "disgust"):
 		urlhere = 'http://www.imdb.com/search/title?genres=musical&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Family genre of
 	# movie against emotion Anger
-	elif(emotion == "Anger"):
+	elif(emotion == "anger"):
 		urlhere = 'http://www.imdb.com/search/title?genres=family&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Thriller genre of
 	# movie against emotion Anticipation
-	elif(emotion == "Anticipation"):
+	elif(emotion == "anticipation"):
 		urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Sport genre of
 	# movie against emotion Fear
-	elif(emotion == "Fear"):
+	elif(emotion == "fear"):
 		urlhere = 'http://www.imdb.com/search/title?genres=sport&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Thriller genre of
 	# movie against emotion Enjoyment
-	elif(emotion == "Enjoyment"):
+	elif(emotion == "enjoyment"):
 		urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Western genre of
 	# movie against emotion Trust
-	elif(emotion == "Trust"):
+	elif(emotion == "trust"):
 		urlhere = 'http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc'
 
 	# IMDb Url for Film_noir genre of
 	# movie against emotion Surprise
-	elif(emotion == "Surprise"):
+	elif(emotion == "surprise"):
 		urlhere = 'http://www.imdb.com/search/title?genres=film_noir&title_type=feature&sort=moviemeter, asc'
 
 	# HTTP request to get the data of
