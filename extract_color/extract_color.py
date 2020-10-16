cdd251b328968cd650bb6f72f3379387e2950cbe
Farhan Syakir <farhan0syakir@gmail.com>
2020-10-16 15:10:12 +0300

    fix flake8 validation

--- extract_color/extract_color.py
+++ extract_color/extract_color.py
@@ -1,21 +1,22 @@
 import cv2
 import numpy as np
 
+
 def extract_color(lower, upper, image):
     # do the color conversion
-    hsv= cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower, upper)
     return mask
 
+
 image = cv2.imread(r'input.jpg')
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
+lower_blue = np.array([110, 50, 50])
+upper_blue = np.array([130, 255, 255])
 
 mask = extract_color(lower_blue, upper_blue, image)
-imask = mask>0
+imask = mask > 0
 blue = np.zeros_like(image, np.uint8)
 blue[imask] = image[imask]
-# cv2.imshow(result)
 cv2.imwrite("result.jpg", blue)
