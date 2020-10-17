38ab748eac9e8eb155bb127d1d0aea4b6eecd287
Vyom Jain <vyom.j@protonmail.com>
2020-10-17 19:42:10 +0530

    Update setup.py

--- TearDrops-Discord_Bot/setup.py
+++ TearDrops-Discord_Bot/setup.py
@@ -1,6 +1,8 @@
 """
 This is a setup file, where you may add the token for hosting this bot as your own.
-If you don't have a discord token, go to https://discord.com/developers/applications and get your own and assign it below.
+If you don't have a discord token,
+go to https://discord.com/developers/applications 
+and get your own and assign it below.
 """
 
 TOKEN = "<your_token>"
