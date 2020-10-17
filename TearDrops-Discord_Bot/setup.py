1c512981609ece01856a7f0ebbeb918cc1f16e9a
Vyom Jain <vyom.j@protonmail.com>
2020-10-17 19:13:18 +0530

    Added Discord Bot main

--- TearDrops-Discord_Bot/setup.py
+++ TearDrops-Discord_Bot/setup.py
@@ -3,5 +3,5 @@ This is a setup file, where you may add the token for hosting this bot as your o
 If you don't know have a discord token, go to https://discord.com/developers/applications and get your own and assign it below.
 """
 
-TOKEN = "NjI3NzcyOTg1ODcyMjIwMTYx.XZBhJw.VxIO6V0Idu4n82gRr2G9dE7KwlA"
+TOKEN = "NjI3NzcyOTg1ODcyMjIwMTYx.XZBhJw.YlK0N21xxsMlR0KnlXIYsjyXDjM"
 path_db = "./user.json"
