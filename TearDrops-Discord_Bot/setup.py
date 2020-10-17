30791164529a4bdee91416cbb8f8b1024a69cde2
Vyom Jain <vyom.j@protonmail.com>
2020-10-17 19:24:26 +0530

    Update setup.py

--- TearDrops-Discord_Bot/setup.py
+++ TearDrops-Discord_Bot/setup.py
@@ -1,7 +1,8 @@
 """
 This is a setup file, where you may add the token for hosting this bot as your own.
-If you don't know have a discord token, go to https://discord.com/developers/applications and get your own and assign it below.
+If you don't have a discord token, go to https://discord.com/developers/applications and get your own and assign it below.
 """
 
-TOKEN = "NjI3NzcyOTg1ODcyMjIwMTYx.XZBhJw.YlK0N21xxsMlR0KnlXIYsjyXDjM"
-path_db = "./user.json"
+TOKEN = "<your_token>"
+USER_NAME = "disbot"
+MONGODB_PASS = "f1xCyKDJfYqSAUnU"
