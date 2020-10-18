a4f31f010119c3867ca2ca5b2bc2eb8724e216e1
habereet <habereet@gmail.com>
2020-10-18 14:08:18 -0400

    updated logic for price checking; returne price

--- amazon-price-alert/amazon_scraper.py
+++ amazon-price-alert/amazon_scraper.py
@@ -158,16 +158,18 @@ def main():
 # get_target_cost validates price input from user
 # Loops once on invalid input
 def get_target_cost(first=True):
+
     try:
-        int(input("Enter your budget price:"))
+        target = int(input("Enter your budget price:"))
     except ValueError:
-        if (first == True):
+        if (first is True):
             print("Please enter only numbers; "
                   "not currency symbols.")
             get_target_cost(first=False)
         else:
             print("ERROR: Your target price wasn't valid")
             exit()
+    return target
 
 
 if __name__ == '__main__':
