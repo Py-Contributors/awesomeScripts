10957e0325a5e1bf5811f54db59972042412b219
khushali452 <khushalivasani.ict19@gmail.com>
2020-10-19 19:29:03 +0530

    changed

--- shape detector/ocv.py
+++ shape detector/ocv.py
@@ -1,13 +1,12 @@
 import cv2
-import numpy as np
 
 font = cv2.FONT_HERSHEY_COMPLEX
-img = cv2.imread("Sample1.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("shapes.jpg", cv2.IMREAD_GRAYSCALE)
 _, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for cnt in contours:
     approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-    cv2.drawContours(img, [approx], 0, (0), 5)
+    cv2.drawContours(img, [approx] , 0, (0), 5 )
     x = approx.ravel()[0]
     y = approx.ravel()[1]
 
@@ -17,7 +16,7 @@ for cnt in contours:
         cv2.putText(img, "Rectangle", (x, y), font, 1, (0))
     elif len(approx) == 5:
         cv2.putText(img, "Pentagon", (x, y), font, 1, (0))
-    elif 6 < len(approx) < 15:
+    elif 6 < len(approx) < 10:
         cv2.putText(img, "Ellipse", (x, y), font, 1, (0))
     else:
         cv2.putText(img, "Circle", (x, y), font, 1, (0))
@@ -25,3 +24,4 @@ cv2.imshow("shapes", img)
 cv2.imshow("Threshold", threshold)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
