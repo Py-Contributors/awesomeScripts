c7b06d90233cf322f4340355c13ca5647618d7d4
Jatin Saini <itsjatin135@gmail.com>
2020-10-22 22:04:48 +0530

    Update voice input output.py

--- voice input output/voice input output.py
+++ voice input output/voice input output.py
@@ -5,25 +5,27 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-	print('Hey Buddy Say Something :')
-	a = r.listen(source)
+    print("Hey Buddy Say Something :")
+    a = r.listen(source)
 
-	try:
-		text = r.recognize_google(a)
-		print(text)
-	except Exception as e:
-		print("sorry")
+    try:
+        text = r.recognize_google(a)
+        print(text)
+    except Exception as e:
+        print(e)
 
 
 # voice output-----
 engine = pt.init()
 
-def speak(sent):
-	engine.say(sent)
-	engine.runAndWait()
-	print(sent)
-	return sent
 
-if __name__ == '__main__':
-  b=str(input("Please enter what to say: "))
-  speak(a)
+def speak(sent):
+    engine.say(sent)
+    engine.runAndWait()
+    print(sent)
+    return sent
+
+
+if __name__ == "__main__":
+    b = str(input("Please enter what to say: "))
+    speak(b)
