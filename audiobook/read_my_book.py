9a5e0c3693e2630315825922c0a19b1b5bdf4c22
Deepak Raj <deepak008@live.com>
2020-10-22 13:11:40 +0530

    improving audiobook

--- audiobook/read_my_book.py
+++ audiobook/read_my_book.py
@@ -1,16 +1,40 @@
+#!/usr/bin/python
+
+import sys
 import pyttsx3
 import PyPDF2
-book = open('sample.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
 
-print("The Book has total: " + str(pages) + " pages!")
 
-start_page = 0
-speaker = pyttsx3.init()
-for num in range(start_page, pages):
-    print("Reading page number " + str(num) + " page!")
-    page = pdfReader.getPage(num)
-    text = page.extractText()
-    speaker.say(text)
-    speaker.runAndWait()
+def get_pdf(book_path):
+    try:
+        book = open(book_path, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(book)
+        return pdfReader
+    except:
+        print("It doesnot look like a Pdf file. may be the path is wrong")        
+
+
+def text_to_speech(book_path):    
+    pdfReader = get_pdf(book_path)
+    try:
+        pages = pdfReader.numPages
+        print("The Book has total: " + str(pages) + " pages!")
+
+        # initiatiazing the pyttsx3 and setting voice speed to 125
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 125)
+
+        start_page = int(input("Please enter the page number: "))
+        for num in range(start_page, pages):
+            print("Reading page number " + str(num) + " page!")
+            page = pdfReader.getPage(num)
+            text = page.extractText()
+            engine.say(text)
+            engine.runAndWait()
+            engine.stop()
+    except:
+        print("Double check the file type or the file path")
+
+if __name__ == "__main__":
+    book_path = sys.argv[1]
+    text_to_speech(book_path)
