06cabed1ebda324aecc611d95310742566907675
AnshikaY <46274445+AnshikaY@users.noreply.github.com>
2020-10-23 18:53:02 +0530

    Update Scrape_Wikipedia_using_speech_recognition.py

--- Scrape_Wikipedia_using_speech_recognition.py
+++ Scrape_Wikipedia_using_speech_recognition.py
@@ -1,18 +1,20 @@
 import pyttsx3
 import speech_recognition as SR
-import wikipedia 
+import wikipedia
 import sys
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # for voice in voices:
 #     print(voice.id)
-# select voice among the available options 
-engine.setProperty('voice', voices[1].id) 
+# select voice among the available options
+# engine.setProperty('voice', voices[1].id)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
 
 def obey_command():
     # It takes input from the microphone and returns output as a string
@@ -21,7 +23,6 @@ def obey_command():
         print("Listening...")
         mic.pause_threshold = 1
         audio = mic.listen(source)
-    
     try:
         print("Recognizing...")
         query = mic.recognize_google(audio, language='en-in')
@@ -33,16 +34,16 @@ def obey_command():
         return "None"
     return query
 
+
 if __name__ == "__main__":
 
     query = obey_command().lower()
 
     if 'wikipedia' in query:
-            speak("Searching wikipedia")
-            query = query.replace("wikipedia", "")
-            result = wikipedia.summary(query, sentences=2)
-            speak("According to wikipedia")
-            speak(result)
-    
+        speak("Searching wikipedia")
+        query = query.replace("wikipedia", "")
+        result = wikipedia.summary(query, sentences=2)
+        speak("According to wikipedia")
+        speak(result)
+
     sys.exit()
-    
