ddc613941df47cc3000e59c8b332a096fba03db1
Xpykerz <51271771+Xpykerz@users.noreply.github.com>
2020-10-28 11:06:00 +0530

    Update Bot.py

--- TelegramEchoBot/Bot.py
+++ TelegramEchoBot/Bot.py
@@ -1,18 +1,24 @@
 import requests
 
-bot_API = 'PAST_YOUR_BOT_API_HERE'
+bot_API = '1048984953:AAGbOkNqYYdGN5jpSoLTy5fTjPcrQWFNsN4'
 
 previous_update_id = None
 
 try:
-    print("Bot is Uprunning now...")
+    print('Bot is Up running now...')
     while(True):
-        message = requests.get(f'https://api.telegram.org/bot{bot_API}/getUpdates').json()
+        message = requests.get(
+            f'https://api.telegram.org/bot{bot_API}/getUpdates'
+            ).json()
         update_id = message['result'][-1]['update_id']
         if update_id != previous_update_id:
-            user = message['result'][-1]['message']['from']['id']
+            user = str(message['result'][-1]['message']['from']['id'])
             replay_message = message['result'][-1]['message']['text']
-            requests.get(f'https://api.telegram.org/bot{bot_API}sendMessage?chat_id={user}&text={replay_message}')
+            requests.get((
+                'https://api.telegram.org/bot'+bot_API +
+                '/sendMessage?chat_id='+user +
+                '&text='+replay_message)
+                    )
             previous_update_id = update_id
-except:
-    print("Somthing Wrong......!!!!!!!!")
+except message.status == 404:
+    print('Somthing Wrong......!!!!!!!!')
