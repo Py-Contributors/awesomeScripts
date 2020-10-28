f4ddc2a10bee1d65be6faffb71e2360b5b1d141f
Xpykerz <51271771+Xpykerz@users.noreply.github.com>
2020-10-28 11:12:12 +0530

    Update Bot.py

--- TelegramEchoBot/Bot.py
+++ TelegramEchoBot/Bot.py
@@ -1,6 +1,6 @@
 import requests
 
-bot_API = '1048984953:AAGbOkNqYYdGN5jpSoLTy5fTjPcrQWFNsN4'
+bot_API = 'PAST_BOT_API_HERE'
 
 previous_update_id = None
 
@@ -9,16 +9,16 @@ try:
     while(True):
         message = requests.get(
             f'https://api.telegram.org/bot{bot_API}/getUpdates'
-            ).json()
+        ).json()
         update_id = message['result'][-1]['update_id']
         if update_id != previous_update_id:
             user = str(message['result'][-1]['message']['from']['id'])
             replay_message = message['result'][-1]['message']['text']
             requests.get((
-                'https://api.telegram.org/bot'+bot_API +
-                '/sendMessage?chat_id='+user +
-                '&text='+replay_message)
-                    )
+                'https://api.telegram.org/bot' + bot_API +
+                '/sendMessage?chat_id=' + user +
+                '&text=' + replay_message
+            ))
             previous_update_id = update_id
 except message.status == 404:
     print('Somthing Wrong......!!!!!!!!')
