fd46f54b3efe7ff30b2c8f240b7c7e44b8b00fab
Amit Mishra <71146681+Mr-Mishraji@users.noreply.github.com>
2020-10-29 21:50:15 +0530

    Update website_blocker.py

--- website_blocker.py
+++ website_blocker.py
@@ -1,12 +1,13 @@
 import time
 from datetime import datetime as dt
-hosts_path = r"C:\Windows\System32\drivers\etc"       # it contains host file it is for windows 
+hosts_path = r "C:\Windows\System32\drivers\etc"       # it contains host file it is for windows 
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com" , " facebook.com" , "www.instagram.com" , " instagram.com"]
 while True:
-    if dt(dt.now().year , dt.now().month , dt.now().day , 5) < dt.now() < dt(dt.now().year , dt.now().month , dt.now().day , 12):
+    if dt(dt.now().year , dt.now().month , dt.now().day , 5)
+    < dt.now() < dt(dt.now().year , dt.now().month , dt.now().day , 12):
         print("Stay focused keep working")
-        with open(hosts_temp , 'r+') as file:
+        with open(hosts_temp, 'r+') as file:
             content = file.read()
             for website in website_list:
                 if website in content:
@@ -14,7 +15,7 @@ while True:
                 else:
                     file.write(redirect + " " + website + "\n")
     else:
-        with open(hosts_temp , 'r+') as file:
+        with open(hosts_temp, 'r+') as file:
             content = file.readlines()      # here content becomes a list
             file.seek(0)
             for line in content:
