629bdc50bcb5830e22345d5d6cfad6d03d39176c
Amit Mishra <71146681+Mr-Mishraji@users.noreply.github.com>
2020-10-29 21:46:34 +0530

    Update website_blocker.py

--- website_blocker.py
+++ website_blocker.py
@@ -4,10 +4,9 @@ hosts_path = r"C:\Windows\System32\drivers\etc"       # it contains host file it
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com" , " facebook.com" , "www.instagram.com" , " instagram.com"]
 while True:
-    if dt(dt.now().year , dt.now().month, dt.now().day, 5) < dt.now() 
-    < dt(dt.now().year , dt.now().month, dt.now().day, 12):
+    if dt(dt.now().year , dt.now().month , dt.now().day , 5) < dt.now() < dt(dt.now().year , dt.now().month , dt.now().day , 12):
         print("Stay focused keep working")
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_temp , 'r+') as file:
             content = file.read()
             for website in website_list:
                 if website in content:
