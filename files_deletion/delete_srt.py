f6a09ff955c07812d653b50de42d8ccff7b284e8
Ronnen Nagal <nagalr@gmail.com>
2021-01-23 11:57:21 +0200

    Python script that delete files with specific extension from the any folder and its sub-folders

---
+++ files_deletion/delete_srt.py
@@ -0,0 +1,12 @@
+# Delete all files ending with .*srt from 'mypath' and all of its subfolders
+# Can be in use for any batch-deletion of similar-extension-files
+import os
+import re
+import os.path
+
+pattern = ".*srt"
+mypath = "/Volumes/MyPassport/Courses/"
+
+for root, dirs, files in os.walk(mypath):
+    for file in filter(lambda x: re.match(pattern, x), files):
+        os.remove(os.path.join(root, file))
