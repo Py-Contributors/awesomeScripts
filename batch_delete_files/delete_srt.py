99b5a5932a3224ffa41790485307c4b321feaa33
Ronnen Nagal <nagalr@gmail.com>
2021-01-23 11:17:53 +0200

    Change to one import per line

--- batch_delete_files/delete_srt.py
+++ batch_delete_files/delete_srt.py
@@ -1,7 +1,9 @@
 # Delete all files ending with .*srt from 'mypath' and all of its subfolders
 # Can be in use for any batch-deletion of similar-extension-files
 
-import os, re, os.path
+import os 
+import re 
+import os.path
 
 pattern = ".*srt"
 mypath = "/Volumes/MyPassport/Courses/"
