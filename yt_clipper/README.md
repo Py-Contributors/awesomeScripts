cc5a449c3c461b1d4319f665951f95d6459898bd
Ezequiel Pássaro <epassaro15@gmail.com>
2021-03-12 03:36:29 -0300

    VA-API hardware acceleration for yt_clipper (#390)
    * add vaapi support for yt_clipper
    
    * add quality options
    
    * check type; set audio codec and bitrate
    
    * update README.md
    
    * minor changes

--- yt_clipper/README.md
+++ yt_clipper/README.md
@@ -1,8 +1,13 @@
 # yt_clipper
 
-This script is intended to be used with `python>=3.6`.
+**This script is currently maintained at: https://github.com/epassaro/yt_clipper**
+
+Easily make audio/video/gif clips from YouTube URLs.
 
 ## Installation
+
+This script is intended to be used with `python>=3.6` and with any Linux distribution.
+However, the script should work on other platforms.
 
 First of all, install `youtube-dl` with `pip`:
 
@@ -10,7 +15,8 @@ First of all, install `youtube-dl` with `pip`:
 pip install --upgrade youtube-dl --user
 ```
 
-Also you will need `ffmpeg` installed on your system.
+Also you will need `ffmpeg` installed on your system. To use VA-API hardware acceleration
+you will need a `ffmpeg` version compiled with VA-API flags enabled.
 
 **On Debian-based distros:**
 
@@ -27,21 +33,31 @@ $ brew install ffmpeg
 ## Usage
 
 ```
-usage: yt_clipper.py [-h] [-s SCALE] [-a] [-g] [-f FPS] [-o OUTPUT] [-q] url start end
+usage: yt_clipper [-h] [-d DEVICE] [-u VALUE] [-x {fast,medium,slow}] [-s SIZE] [-c {aac,mp3}] [-b {96k,128k,192k,256k,320k}] [-a] [-g] [-f FPS] [-o FILENAME] [-q]
+                  url start end
 
-make clips from youtube videos
+easily make audio/video/gif clips from YouTube URLs
 
 positional arguments:
-  url                           video url
-  start                         HH:MM:SS
-  end                           HH:MM:SS
+  url                             video url
+  start                           HH:MM:SS.ms
+  end                             HH:MM:SS.ms
 
 optional arguments:
-  -h, --help                    show this help message and exit
-  -s SCALE, --scale SCALE       scale image vertically (in px)
+  -h, --help                      show this help message and exit
+  -d DEVICE, --device DEVICE      VA-API hardware acceleration device (experimental), for example: '/dev/dri/render128D'
+  -u VALUE, --quality VALUE       from 0 to 51, default is 23 (lower is better)
+  -x {fast,medium,slow}, --compression {fast,medium,slow}
+                                  encoder preset (cpu only)
+  -s SIZE, --scale SIZE           scale image vertically (in px)
+  -c {aac,mp3}, --audio-codec {aac,mp3}
+  -b {96k,128k,192k,256k,320k}, --bitrate {96k,128k,192k,256k,320k}
+                                  audio constant bitrate (CBR)
   -a, --audio-only
   -g, --gif
-  -f FPS, --fps FPS             gif fps
-  -o OUTPUT, --output OUTPUT
+  -f FPS, --fps FPS               gif fps
+  -o FILENAME, --output FILENAME  custom file name and container
   -q, --quiet
+
 ```
+
--- yt_clipper/yt_clipper.py
+++ yt_clipper/yt_clipper.py
@@ -1,12 +1,38 @@
 #! /usr/bin/env python3
+#
+# ---------------------------------------------------
+# Easily make audio/video/gif clips from YouTube URLs
+#
+# (C) 2020-2021 Ezequiel Pássaro
+# Released under MIT License
+#
+# github: github.com/epassaro
+# email: epassaro15@gmail.com
+# ---------------------------------------------------
+#
 
 import re
 import argparse
 import subprocess
+from datetime import datetime
+
+
+def valid_time_type(arg):
+    """Custom argparse type for time values."""
+    try:
+        if "." not in arg:
+            return datetime.strptime(arg, "%H:%M:%S").strftime("%H:%M:%S")
+        else:
+            return datetime.strptime(arg, "%H:%M:%S.%f").strftime("%H:%M:%S.%f")
+    except ValueError:
+        msg = f"{arg} not valid, expected format: 'HH:MM:SS.ms'"
+        raise argparse.ArgumentTypeError(msg)
 
 
 def find_url(string):
-    """ Finds an arbitrary number of URLs in a string and returns them in a list.
+    """Finds an arbitrary number of URLs in a string and returns them in a list.
+    `youtube-dl` returns two URLs, one for audio and other for video.
+
     Taken from: https://www.geeksforgeeks.org/python-check-url-string/"""
     regex = (
         r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s"
@@ -18,73 +44,138 @@ def find_url(string):
     return [x[0] for x in url]
 
 
-# Parse arguments
-parser = argparse.ArgumentParser(description="make clips from youtube videos")
-parser.add_argument("url", help="video url")
-parser.add_argument("start", help="HH:MM:SS")
-parser.add_argument("end", help="HH:MM:SS")
-parser.add_argument("-s", "--scale", help="scale image vertically (in px)",
-                    type=int, default=-2)
-parser.add_argument("-a", "--audio-only", action="store_true",
-                    dest="audio_only")
-parser.add_argument("-g", "--gif", action="store_true")
-parser.add_argument("-f", "--fps", type=int, default=12, help="gif fps")
-parser.add_argument("-o", "--output")
-parser.add_argument("-q", "--quiet", action="store_true")
-args = parser.parse_args()
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description="easily make audio/video/gif clips from YouTube URLs"
+    )
+    parser.add_argument("url", help="video url", type=str)
+    parser.add_argument("start", help="HH:MM:SS.ms", type=valid_time_type)
+    parser.add_argument("end", help="HH:MM:SS.ms", type=valid_time_type)
+    parser.add_argument(
+        "-d",
+        "--device",
+        help="device for VA-API hardware acceleration (experimental), for example: \
+                '/dev/dri/render128D'",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "-u",
+        "--quality",
+        help="from 0 to 51, default is 23 (lower is better)",
+        type=int,
+        default=23,
+        metavar="VALUE",
+    )
+    parser.add_argument(
+        "-x",
+        "--compression",
+        help="encoder preset (cpu only)",
+        choices=["fast", "medium", "slow"],
+        default="medium",
+    )
+    parser.add_argument(
+        "-s",
+        "--scale",
+        help="scale image vertically (in px)",
+        type=int,
+        default=-2,
+        metavar="SIZE",
+    )
+    parser.add_argument(
+        "-c",
+        "--audio-codec",
+        choices=["aac", "mp3"],
+        default="aac",
+    )
+    parser.add_argument(
+        "-b",
+        "--bitrate",
+        help="audio constant bitrate (CBR)",
+        choices=["96k", "128k", "192k", "256k", "320k"],
+        default="128k",
+    )
+    parser.add_argument("-a", "--audio-only", action="store_true", dest="audio_only")
+    parser.add_argument("-g", "--gif", action="store_true")
+    parser.add_argument("-f", "--fps", help="gif fps", type=int, default=12)
+    parser.add_argument(
+        "-o", "--output", help="custom file name and container", metavar="FILENAME"
+    )
+    parser.add_argument("-q", "--quiet", action="store_true")
+    args = parser.parse_args()
 
-# Get video ID to use as default file name
-get_id = subprocess.Popen(
-    ["youtube-dl", "--get-id", args.url],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-)
-
-video_id = None
-while not video_id:  # Sometimes `get_id` returns nothing
-    out, err = get_id.communicate()
-    get_id.wait()
-    video_id = out.strip()  # Remove newlines
-
-# Get video and audio URLs
-get_url = subprocess.Popen(
-    ["youtube-dl", "-g", args.url],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-)
-
-out, err = get_url.communicate()
-get_url.wait()
-video_url, audio_url = find_url(out)
-
-# Define ffmpeg commands
-ffmpeg_cmd = f"ffmpeg -ss {args.start} -to {args.end} -i {video_url} -ss {args.start}\
-                -to {args.end} -i {audio_url} -map 0:v -map 1:a -c:v libx264 -c:a aac\
-                    -vf scale={args.scale}:-2:flags=lanczos -y {video_id}.mp4"
-
-if args.audio_only:
-    ffmpeg_cmd = f"ffmpeg -ss {args.start} -to {args.end} -i {audio_url} -c:a aac -y\
-                    {video_id}.aac"
-
-if args.gif:
-    ffmpeg_cmd = (
-        f"ffmpeg -ss {args.start} -to {args.end} -i {video_url}"
-        f" -filter_complex [0:v]fps={args.fps},scale={args.scale}:-2"
-        ":flags=lanczos,split[a][b];[a]palettegen[p];[b][p]paletteuse"
-        f" -y {video_id}.gif"
+    # Get video ID to use as default file name
+    get_id = subprocess.Popen(
+        ["youtube-dl", "--get-id", args.url],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
     )
 
-# Split command in a list to use later with subprocess.Popen
-ffmpeg_args = ffmpeg_cmd.split()
+    video_id = None
+    while not video_id:  # Sometimes `get_id` returns nothing
+        out, err = get_id.communicate()
+        get_id.wait()
+        video_id = out.strip()  # Remove newlines
 
-if args.output:
-    ffmpeg_args[-1] = args.output
+    # Get video and audio URLs
+    get_url = subprocess.Popen(
+        ["youtube-dl", "-g", args.url],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
 
-if args.quiet:
-    ffmpeg_args = ffmpeg_args + ["-v", "fatal"]
+    out, err = get_url.communicate()
+    get_url.wait()
+    video_url, audio_url = find_url(out)
 
-# Run ffmpeg
-make_clip = subprocess.Popen(ffmpeg_args)
-make_clip.wait()
+    # Set default parameters (video)
+    if args.device:
+        video_codec = "h264_vaapi"
+        vaapi = f"-hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device \
+                    {args.device}"
+        scale = f"scale_vaapi={args.scale}:-2"
+        quality = f"-qp {args.quality}"
+
+    else:
+        video_codec = "libx264"
+        vaapi = ""
+        scale = f"scale={args.scale}:-2:flags=lanczos"
+        quality = f"-preset {args.compression} -crf {args.quality}"
+
+    # Define ffmpeg commands
+    if args.audio_only:
+        ffmpeg_cmd = f"ffmpeg -ss {args.start} -to {args.end} -i {audio_url} \
+                        -c:a {args.audio_codec} -b:a {args.bitrate} \
+                            -y {video_id}.{args.audio_codec}"
+
+    elif args.gif:
+        ffmpeg_cmd = (
+            f"ffmpeg -ss {args.start} -to {args.end} -i {video_url}"
+            f" -filter_complex [0:v]fps={args.fps},{scale},"
+            "split[a][b];[a]palettegen[p];[b][p]paletteuse"
+            f" -y {video_id}.gif"
+        )
+
+    else:
+        ffmpeg_cmd = f"ffmpeg {vaapi} \
+                        -ss {args.start} -to {args.end} -i {video_url} \
+                            -ss {args.start} -to {args.end} -i {audio_url} \
+                                -c:v {video_codec} {quality} -vf {scale} \
+                                    -c:a {args.audio_codec} -b:a {args.bitrate} \
+                                        -map 0:v -map 1:a -y {video_id}.mp4"
+
+    # Split command in a list to use later with subprocess.Popen
+    ffmpeg_args = ffmpeg_cmd.split()
+
+    if args.output:
+        ffmpeg_args[-1] = args.output
+
+    if args.quiet:
+        ffmpeg_args = ffmpeg_args + ["-v", "fatal"]
+
+    # Run ffmpeg
+    make_clip = subprocess.Popen(ffmpeg_args)
+    make_clip.wait()
