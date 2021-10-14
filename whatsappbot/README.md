67a168bfdc2d6b7c3034b9470a19a5ddf2db3519
aditya <extracorrection@gmail.com>
2021-10-14 05:47:55 -0700

    added whatsapp auto reponse bot using python project

---
+++ whatsappbot/README.md
@@ -0,0 +1,23 @@
+
+ # Whatsapp auto response bot
+This is a python mini project (bot) which reads the recent chats of contacts , if chats matches with response dictionary it reply accordingly.
+
+# Custom responses
+change this according to you :)
+
+```python
+....
+response = {
+  "message received": "message to be sent",
+} 
+...
+```
+## Installation
+You need to install some packages for this project.Run this commands in your terminal to install :)
+
+```bash
+pip install -r requirements.txt
+```
+
+## SETUP
+you have to download [chromedriver ](https://chromedriver.chromium.org/downloads) and replace the file (chromedriver.exe) with downloaded one but If you are using Chrome version 95 ,no need to download anything. [check your chrome version ](chrome://settings/help)
---
+++ whatsappbot/main.py
@@ -0,0 +1,61 @@
+#importing necessary packages
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+#setup for chromedriver
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument('--remote-debugging-port=9222')
+driver = webdriver.Chrome(executable_path=r"chromedriver.exe",options=options)
+driver.get("https://web.whatsapp.com/")
+input("Press anything after QR scan")
+time.sleep(5)
+
+#code for getting all recent contacts
+contact_list=[]
+for contacts in driver.find_elements_by_class_name('_21xgG'):   
+    #looping through each contact name 
+    title = contacts.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
+    contact_list+=list(title.split('\n'))   
+print(contact_list)
+
+#response Dictionaries
+#add this according to you 
+response = {
+  "hii": "hello there we are unavaiable right now leave your message",
+  "how are you": "i am fine 😁",
+  "bhai paisa de de": "nikallllllllll"
+} 
+
+#reading recent chats of every person on list
+for name in contact_list:
+    try:
+        person = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+        person.click()
+        time.sleep(3)
+    except Exception:
+        continue
+           
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")   
+    msg_got =driver.find_elements_by_css_selector(".GDTQm.message-in.focusable-list-item")
+    msg = [message.text for message in msg_got]
+    print(msg)
+    if msg==[]:
+        msg=['none']
+    
+    #sending response according to message
+    for key,value in response.items():       
+        if key in msg[-1]:
+            print(key,value)
+            reply=driver.find_element_by_class_name("_2A8P4")                    
+            actions =ActionChains(driver)
+            actions.send_keys(value + Keys.ENTER)            
+            actions.perform()
+            time.sleep(10)      
+            print('MESSAGE FOUND :)')
+        else:       
+            print('No one message you :( ')
+driver.quit()
