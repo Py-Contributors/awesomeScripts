2d29fd71145536ef93bc00fdcb6250411a9cf0ae
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 02:50:32 +0800

    Delete audiobook directory

--- audiobook/read_my_book.py
+++
@@ -1,7 +0,0 @@
-# This code has been moved to new repo
-# https://github.com/codePerfectPlus/audiobook
-
-from audiobook import AudioBook
-
-ab = AudioBook("audiobook/sample.pdf")
-ab.text_to_speech()
