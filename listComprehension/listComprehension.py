c3b20c7cd60a2b972d3e61eb14cc27174ab85fa3
Archit Gupta <guptaarchit137@gmail.com>
2022-10-01 20:35:04 +0530

    Created Code file
    Added code to the file

---
+++ listComprehension/listComprehension.py
@@ -0,0 +1,10 @@
+# Create a list of characters using list comprehension
+char_list = [ char for char in "linuxhint" ]
+print(char_list)
+
+# Define a tuple of websites
+websites = ("google.com","yahoo.com", "ask.com", "bing.com")
+
+# Create a list from tuple using list comprehension
+site_list = [ site for site in websites ]
+print(site_list)
