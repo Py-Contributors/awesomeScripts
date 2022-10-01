d6a8c15b829f41c68fe116f46f0a283058919832
Aayushi Sharma <59429424+aayushi-droid@users.noreply.github.com>
2022-10-01 19:26:47 +0530

    Update CheckUsername.py (#472)

--- check_github_username/CheckUsername.py
+++ check_github_username/CheckUsername.py
@@ -1,25 +1,32 @@
 import sys
 import requests
+import argparse
+
+arg = argparse.ArgumentParser()
+arg.add_argument('-u', '--username', help='Username to check')
+args = arg.parse_args()
 
 API_URI = 'https://api.github.com/users'
 
-
-def main(argv):
-    u = argv[1]
-    print(f'Searching username "{u}"')
-    res = requests.get(f'{API_URI}/{u}')
+def check_username_available(username: str):
+    print('Searching username {}'.format(username))
+    res = requests.get('{}/{}'.format(API_URI, username))
 
     if res.status_code == 404:  # Not found
-        print('Username is available :D', end='')
+        return True
+    elif res.status_code == 200:
+        return False
     else:
-        print('Username is already in use :(', end='')
-    # print(f' -- ({res.status_code})')
-    print()
+        raise Exception('Error: {}'.format(res.status_code))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Missing <username> parameter', file=sys.stderr)
-        exit(1)
+    username = args.username
+    output = check_username_available(username)
+    if output:
+        print('Username is available :)', end='')
     else:
-        main(sys.argv)
+        print('Username is already in use :(', end='')
+        
+    
+    
